8c0a7b6619e7a4fe2f9ce95408e61aff6aac1d5e
Саша Булаев <paada@MacBook-Pro-Sasa.local>
2023-03-26 00:18:52 +0300

    add sass support

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 console.log('34234234234');
+import styles from  './styles.module.css'
+import styles2 from  './stylestwo.module.scss'
+// let a = [1,2,3].map(x => x + 1)
+// let b = [1,2,3].at(-2)
+// console.log(styles);
+// console.log(a,b );
 
-let a = [1,2,3].map(x => x + 1)
-let b = [1,2,3].at(-2)
-
-console.log(a,b );
-// document.body.textContent = a;
+setTimeout(() => document.body.classList.add(styles2.red), 1)
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,14 @@ const path = require('path')
 
 module.exports = {
     entry: './src/main.js',
+    mode: "production",
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: 'bundle.js'
     },
+    plugins: [
+        {}
+    ],
 
     module: {
         rules: [
@@ -16,17 +20,44 @@ module.exports = {
                     loader: 'babel-loader',
                 },
             },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules:{
+                                localIdentName: "[local]_[hash:base64:5]",
+                            }
+                        }
+                    },
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules:{
+                                localIdentName: "[local]_[hash:base64:5]",
+                            }
+                        }
+                    },
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
         ],
+
     },
     devServer: {
         static: {
             directory: path.resolve(__dirname, './dist')
         }
-    },
-    experiments: {
-        futureDefaults: true,
-    },
-    node: {
-        global: true
     }
 }
